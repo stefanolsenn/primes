485a074b895ab4e30d8dfb6d68fb68fdccb91402
mashtoe <mashtoe@hotmail.com>
2019-10-10 14:36:44 +0200

    gui

--- primes/Program.cs
+++ primes/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using Prime.logic;
 
@@ -13,13 +14,84 @@ namespace primes
         static async Task Main()
         {
             var program = new Program();
-            long first = 10;
-            long last = 100_000_0;
-            Console.Write("Primes seq: ");
-            var seq = program.Watch(() => program._generator.GetPrimesSequential(first, last));
-            Console.WriteLine();
             
 
+            var seq = program.RunTaskInParallel();
+            Console.Write("\rPrimes seq: ");
+
+            Console.WriteLine("done");
+            var seqRes = seq.Result;
+            //for (int j = 0; j < seqRes.Count; j = j + 1000) {
+            //    Console.Write(seqRes[j] + " ");
+            //}
+            //var seq = program.Watch(() => program._generator.GetPrimesSequential(userInput[0], userInput[1]));
+            Console.WriteLine();
+            Console.ReadLine();   
+
+        }
+
+        Task<List<long>> RunTaskInParallel() {
+            long first = 10;
+            long last = 100_000_0;
+            var userInput = GetRangeInput();
+
+            var seq = StartTask(userInput[0], userInput[1]);
+            var i = 0;
+            while (seq.Status != TaskStatus.RanToCompletion) {
+                switch (i) {
+                    case 0:
+                        Console.Write("\r|");
+                        i++;
+                        break;
+                    case 1:
+                        Console.Write("\r/");
+                        i++;
+                        break;
+                    case 2:
+                        Console.Write("\r_");
+                        i++;
+                        break;
+                    case 3:
+                        Console.Write("\r\\");
+                        i++;
+                        break;
+                    case 4:
+                        Console.Write("\r|");
+                        i++;
+                        break;
+                    case 5:
+                        Console.Write("\r/");
+                        i++;
+                        break;
+                    case 6:
+                        Console.Write("\r_");
+                        i++;
+                        break;
+                    case 7:
+                        Console.Write("\r\\");
+                        i = 0;
+                        break;
+                }
+                Thread.Sleep(100);
+            }
+            return seq;
+        }
+
+        async Task<List<long>> StartTask(long first, long last) {
+            var seq = await Task.Run(() => Watch(() => _generator.GetPrimesSequential(first, last)));
+            //var seq = program.Watch(() => program._generator.GetPrimesSequential(userInput[0], userInput[1]));
+
+            return seq;
+        }
+
+        long[] GetRangeInput() {
+            long[] set = new long[2];
+            var first = long.Parse( Console.ReadLine());
+            var last = long.Parse( Console.ReadLine());
+            set[0] = first;
+            set[1] = last;
+            return set;
+            
         }
 
         public T Watch<T>(Func<T> action)
