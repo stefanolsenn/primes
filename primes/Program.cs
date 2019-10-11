d6df4da8ac961362725d21a36f1a755ede95b8e0
mashtoe <mashtoe@hotmail.com>
2019-10-11 09:52:01 +0200

    gui stuff done

--- primes/Program.cs
+++ primes/Program.cs
@@ -14,71 +14,90 @@ namespace primes
         static async Task Main()
         {
             var program = new Program();
-            
 
-            var seq = program.RunTaskInParallel();
-            Console.Write("\rPrimes seq: ");
+            var userInput = program.GetRangeInput();
+            Console.Write("Primes seq: ");
+            Console.WriteLine();
+            var seq = program.StartTask(userInput[0],userInput[1]);
+            program.ShowGuiStuff(userInput[0],userInput[1], seq);
+            //var seqRes = seq.Result;
 
-            Console.WriteLine("done");
-            var seqRes = seq.Result;
-            //for (int j = 0; j < seqRes.Count; j = j + 1000) {
-            //    Console.Write(seqRes[j] + " ");
-            //}
-            //var seq = program.Watch(() => program._generator.GetPrimesSequential(userInput[0], userInput[1]));
+            Console.Write("Parallel primes seq: ");
+            Console.WriteLine();
+            var seq2 = program.StartTaskPrimesParallel(userInput[0], userInput[1]);
+            program.ShowGuiStuff(userInput[0], userInput[1], seq2);
+            //var seqRes = seq.Result;
+
+            Console.Write("Parallel partitioned primes seq: ");
+            Console.WriteLine();
+            var seq3 = program.StartTaskPrimesParallelPartitioned(userInput[0], userInput[1]);
+            program.ShowGuiStuff(userInput[0], userInput[1], seq3);
+            //var seqRes = seq.Result;
+
             Console.WriteLine();
             Console.ReadLine();   
 
         }
 
-        Task<List<long>> RunTaskInParallel() {
+        void ShowGuiStuff(long start, long end, Task task) {
             long first = 10;
             long last = 100_000_0;
-            var userInput = GetRangeInput();
 
-            var seq = StartTask(userInput[0], userInput[1]);
             var i = 0;
-            while (seq.Status != TaskStatus.RanToCompletion) {
+            bool reset = false;
+            while (task.Status != TaskStatus.RanToCompletion) {
                 switch (i) {
                     case 0:
                         Console.Write("\r|");
-                        i++;
                         break;
                     case 1:
                         Console.Write("\r/");
-                        i++;
                         break;
                     case 2:
-                        Console.Write("\r_");
-                        i++;
+                        Console.Write("\r-");
                         break;
                     case 3:
                         Console.Write("\r\\");
-                        i++;
                         break;
                     case 4:
                         Console.Write("\r|");
-                        i++;
                         break;
                     case 5:
                         Console.Write("\r/");
-                        i++;
                         break;
                     case 6:
-                        Console.Write("\r_");
-                        i++;
+                        Console.Write("\r-");
                         break;
                     case 7:
                         Console.Write("\r\\");
-                        i = 0;
+                        reset = true;
                         break;
+                }
+                if (reset) {
+                    i = 0;
+                    reset = false;
+                } else {
+                    i++;
                 }
                 Thread.Sleep(100);
             }
+        }
+
+        async Task<List<long>> StartTaskPrimesParallelPartitioned(long first, long last) {
+            var seq = await Task.Run(() => Watch(() => _generator.GetPrimesParallelPartitioned(first, last)));
+
+            return seq;
+        }
+
+        async Task<List<long>> StartTaskPrimesParallel(long first, long last) {
+            var seq = await Task.Run(() => Watch(() => _generator.GetPrimesParallel(first, last)));
+
             return seq;
         }
 
         async Task<List<long>> StartTask(long first, long last) {
             var seq = await Task.Run(() => Watch(() => _generator.GetPrimesSequential(first, last)));
+            //var seq = await Task.Run(() => Watch(() => _generator.GetPrimesSequential(first, last)));
             //var seq = program.Watch(() => program._generator.GetPrimesSequential(userInput[0], userInput[1]));
 
             return seq;
@@ -100,7 +119,8 @@ namespace primes
             sw.Start();
             var result = action.Invoke();
             sw.Stop();
-            Console.Write($"{sw.ElapsedMilliseconds / 1000d :F5} sek");
+            Console.SetCursorPosition(35, Console.CursorTop -1);
+            Console.WriteLine($"{sw.ElapsedMilliseconds / 1000d :F5} sek");
             return result;
         }
         
